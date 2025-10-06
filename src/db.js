b88a05023c3c3a6f880c96a540b3c43a59f7ce54
Guilherme Henrique <100978223+ghps2704@users.noreply.github.com>
2025-10-06 12:08:35 -0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'Gui271004?',
+  password: '',
   database: 'hospital_db',
 });
 
